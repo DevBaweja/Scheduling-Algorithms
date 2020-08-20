92f17d6a7fce63a74bb87cc80c4f33f05689c96b
unknown <18ucs081@lnmiit.ac.in>
2020-08-21 01:25:56 +0530

    readFile Header file and exposure of PID,AT,BT from respective file

--- src/first-come-first-serve/FCFS-NPE.c
+++ src/first-come-first-serve/FCFS-NPE.c
@@ -4,3 +4,12 @@ First Come First Serve
 Criteria: Arrival Time
 Mode: Non-Preemptive
 */
+#include <stdio.h>
+
+#include "../utils/read-file.h"
+
+int main()
+{
+    readFile("FCFS-NPE.csv");
+    return 0;
+}
---
+++ src/utils/read-file.h
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char **PID;
+char **AT;
+char **BT;
+int TOTAL;
+
+void readFile(char path[])
+{
+
+    char filename[] = "../../input/";
+    strcat(filename, path);
+    FILE *fp, *fptemp;
+
+    fp = fopen(filename, "r");
+    fptemp = fopen(filename, "r");
+
+    if (!fp && !fptemp)
+    {
+        printf("Cann't open file!");
+        exit(0);
+    }
+
+    int SIZE = 1000;
+    char temp[SIZE];
+    TOTAL = 0;
+    while (strcmp(fgets(temp, sizeof(temp), (FILE *)fptemp), "EOF") != 0)
+        TOTAL++;
+    PID = (char **)malloc(TOTAL + 1);
+    AT = (char **)malloc(TOTAL + 1);
+    BT = (char **)malloc(TOTAL + 1);
+
+    // Read content from file
+    char content[SIZE];
+    for (int pos = 0; strcmp(fgets(content, sizeof(content), (FILE *)fp), "EOF") != 0; pos++)
+    {
+        char *token;
+        token = strtok(content, ":");
+        PID[pos] = (char *)malloc(strlen(token) + 1);
+        strcpy(PID[pos], token);
+
+        char *args[2];
+        // Spliting with delimiter of :
+        for (int index = 0; (token = strtok(NULL, ":")) != NULL; index++)
+            args[index] = token;
+
+        // Memory Allocation
+        AT[pos] = (char *)malloc(strlen(args[0]) + 1);
+        strcpy(AT[pos], args[0]);
+
+        BT[pos] = (char *)malloc(strlen(args[1]) + 1);
+        strcpy(BT[pos], args[1]);
+    }
+    // Close file
+    fclose(fp);
+}
